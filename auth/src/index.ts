8de49b492abd3a9395e5326126e3eb28b4348691
isadora lopes <isadora.c.lopes@ibm.com>
2023-08-20 13:52:30 -0300

    Creating Route Handlers

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,14 +1,14 @@
 import express from "express";
 import { json } from "body-parser";
 
+import { currentuserRouter } from "./routes/current-user";
+
 const app = express();
 app.use(json());
 
 const PORT = 3000;
 
-app.get('/api/users/currentuser', (req, res) => {
-    res.send('...');
-})
+app.use(currentuserRouter);
 
 app.listen(PORT, () => {
     console.log('Listening on port', PORT)
