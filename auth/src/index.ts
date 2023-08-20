2354abbb0dd8bc5de33e08f12bd626d53217f8f0
isadora lopes <isadora.c.lopes@ibm.com>
2023-08-20 15:13:48 -0300

    Building an Error Handling Middleware

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,6 +5,7 @@ import { currentuserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
 import { signupRouter } from "./routes/signup";
+import { errorHandler } from "./middlewares/error-handler";
 
 const app = express();
 app.use(json());
@@ -15,6 +16,8 @@ app.use(currentuserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
     console.log('Listening on port', PORT)
