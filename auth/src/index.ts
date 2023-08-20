1c57d734a15d4d3b9b4049ba8b3e27691c3e4ea4
isadora lopes <isadora.c.lopes@ibm.com>
2023-08-20 13:57:23 -0300

    Scaffolding Routes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,9 @@ import express from "express";
 import { json } from "body-parser";
 
 import { currentuserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { signuptRouter } from "./routes/signup";
 
 const app = express();
 app.use(json());
@@ -9,6 +12,9 @@ app.use(json());
 const PORT = 3000;
 
 app.use(currentuserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signuptRouter);
 
 app.listen(PORT, () => {
     console.log('Listening on port', PORT)
