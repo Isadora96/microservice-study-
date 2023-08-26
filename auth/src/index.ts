18fc6cfc8b277121acfaea49cb263d9eafe8f919
isadora lopes <isadora.c.lopes@ibm.com>
2023-08-26 20:36:00 -0300

    Connecting to MongoDB

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
+import mongoose from "mongoose";
 
 import { currentuserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
@@ -25,6 +26,17 @@ app.all('*', () => {
 
 app.use(errorHandler);
 
-app.listen(PORT, () => {
-    console.log('Listening on port', PORT)
-})
+const start = async () => {
+    try {
+        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+        console.log('Connected to MongoDb');
+    } catch(err) {
+        console.log(err)
+    }
+
+    app.listen(PORT, () => {
+        console.log('Listening on port', PORT)
+    });
+};
+
+start();
