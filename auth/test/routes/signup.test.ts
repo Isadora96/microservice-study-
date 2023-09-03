e3126ae6d51264aa3990393f92398720ef6113b4
isadora lopes <isadora.c.lopes@ibm.com>
2023-09-03 16:43:08 -0300

    Testing Invalid Input

--- auth/test/routes/signup.test.ts
+++ auth/test/routes/signup.test.ts
@@ -1,6 +1,9 @@
 import request from "supertest";
 import { app } from "../../src/app";
 
+const invalidEmailMsg = { errors: [ { message: 'Email must be valid!', field: 'email' }] };
+const invalidPasswordMsg = { errors: [ { message: 'Password must be between 4 and 20 characters!', field: 'password' }] };
+
 it('returns a 201 on successful signup', async () => {
     return request(app)
         .post('/api/users/signup')
@@ -9,4 +12,55 @@ it('returns a 201 on successful signup', async () => {
             password: "password"
         })
         .expect(201);
+});
+
+it('returns a 400 with an invalid email', async () => {
+    return request(app)
+        .post('/api/users/signup')
+        .send({
+            email: "bad@email",
+            password: "password"
+        })
+        .expect(400)
+        .expect(invalidEmailMsg);
+});
+
+it('returns a 400 with an invalid password (less than 4) ', async () => {
+    return request(app)
+        .post('/api/users/signup')
+        .send({
+            email: "test@test.com",
+            password: "sl"
+        })
+        .expect(400)
+        .expect(invalidPasswordMsg);
+});
+
+it('returns a 400 with an invalid password (more than 20) ', async () => {
+    return request(app)
+        .post('/api/users/signup')
+        .send({
+            email: "test@test.com",
+            password: "areallylonginvalidpassword"
+        })
+        .expect(400)
+        .expect(invalidPasswordMsg);
+});
+
+it('returns a 400 with missing email and password', async () => {
+    await request(app)
+        .post('/api/users/signup')
+        .send({
+            email: "test@test.com"
+        })
+        .expect(400)
+        .expect(invalidPasswordMsg);
+
+    await request(app)
+        .post('/api/users/signup')
+        .send({
+            password: "password"
+        })
+        .expect(400)
+        .expect(invalidEmailMsg);
 });
