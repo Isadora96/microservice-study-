afca3a732780d0366e4ec81852aac1f8f8f2701b
isadora lopes <isadora.c.lopes@ibm.com>
2023-09-03 16:54:59 -0300

    Requiring Unique Emails test

--- auth/test/routes/signup.test.ts
+++ auth/test/routes/signup.test.ts
@@ -64,3 +64,22 @@ it('returns a 400 with missing email and password', async () => {
         .expect(400)
         .expect(invalidEmailMsg);
 });
+
+it('disallows duplicate emails', async () => {
+    await request(app)
+        .post('/api/users/signup')
+        .send({
+            email: "test@test.com",
+            password: "password"
+        })
+        .expect(201)
+
+    await request(app)
+        .post('/api/users/signup')
+        .send({
+            email: "test@test.com",
+            password: "password"
+        })
+        .expect(400)
+        .expect({});
+});
