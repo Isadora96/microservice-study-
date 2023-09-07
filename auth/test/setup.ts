8e1be4768fa5c97a4f581003853dde945ce4b5bc
isadora lopes <isadora.c.lopes@ibm.com>
2023-09-07 13:24:03 -0300

    Auth Helper Function

--- auth/test/setup.ts
+++ auth/test/setup.ts
@@ -1,5 +1,11 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
+import request from "supertest";
 import mongoose from "mongoose";
+
+import { app } from "../src/app";
+declare global {
+    var singupGetCookie: () => Promise<string[]>;
+}
 
 let mongo: any;
 beforeAll(async () => {
@@ -25,3 +31,17 @@ afterAll(async () => {
     }
     await mongoose.connection.close();
 });
+
+global.singupGetCookie =async () => {
+    
+    const email = "test@test.com";
+    const password = "password";
+
+    const signupResponse = await request(app)
+        .post('/api/users/signup')
+        .send({ email, password })
+        .expect(201);
+
+    return signupResponse.get('Set-Cookie');
+    
+}
