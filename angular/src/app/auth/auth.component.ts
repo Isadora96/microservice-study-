ae7ac8d1dca4dc9f62d686bb5ba87f135af4fe39
isadora lopes <isadora.c.lopes@ibm.com>
2023-09-10 19:32:13 -0300

    Handling Email and Password Inputs

--- angular/src/app/auth/auth.component.ts
+++ angular/src/app/auth/auth.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from "@angular/forms";
 
 @Component({
   selector: 'app-auth',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./auth.component.css']
 })
 export class AuthComponent {
+  constructor () {}
 
+  onSubmit(form: NgForm) {
+
+    const email = form.value.email;
+    const password = form.value.password;
+
+    console.log(email, password);
+  }
 }
