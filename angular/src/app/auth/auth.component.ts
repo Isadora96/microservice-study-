27bc6edbc1cb30c8627f183c54d4ec1def3b4063
isadora lopes <isadora.c.lopes@ibm.com>
2023-09-10 19:55:31 -0300

    Successful Account Signup

--- angular/src/app/auth/auth.component.ts
+++ angular/src/app/auth/auth.component.ts
@@ -1,19 +1,25 @@
 import { Component } from '@angular/core';
 import { NgForm } from "@angular/forms";
 
+import { AuthService } from "./auth.service";
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.css']
 })
 export class AuthComponent {
-  constructor () {}
+  constructor (private authService: AuthService) {}
 
   onSubmit(form: NgForm) {
 
     const email = form.value.email;
     const password = form.value.password;
 
-    console.log(email, password);
+    this.authService.signUp(email, password).then(response => {
+      console.log(response.data)
+    }).catch(err => {
+      console.error(err);
+    });
+
   }
 }
