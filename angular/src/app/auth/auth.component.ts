8143400309d35c1749902877c108df0d64928dc2
isadora lopes <isadora.c.lopes@ibm.com>
2023-09-10 20:57:48 -0300

    Handling Validation Errors

--- angular/src/app/auth/auth.component.ts
+++ angular/src/app/auth/auth.component.ts
@@ -8,6 +8,10 @@ import { AuthService } from "./auth.service";
   styleUrls: ['./auth.component.css']
 })
 export class AuthComponent {
+
+  emailErrorMsg: string = '';
+  passwordErrorMsg: string = '';
+
   constructor (private authService: AuthService) {}
 
   onSubmit(form: NgForm) {
@@ -18,8 +22,19 @@ export class AuthComponent {
     this.authService.signUp(email, password).then(response => {
       console.log(response.data)
     }).catch(err => {
-      console.error(err);
+      this.showErrorMessage(err.response.data.errors);
     });
+
+  }
+
+  private showErrorMessage(message: Array<{message: string, field: string}>) {
+      message.forEach(value => {
+        if(value.field == 'email' || value.message.toLocaleLowerCase().includes('email')) {
+          this.emailErrorMsg = value.message
+        } else if (value.field == 'password' || value.message.toLocaleLowerCase().includes('password')) {
+          this.passwordErrorMsg = value.message
+        }
+      })
 
   }
 }
